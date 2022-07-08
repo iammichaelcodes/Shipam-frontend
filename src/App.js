f613466356aa05b17a8c0e9426420b5e47a05d59
iammichaelcodes <mikeevboumwan52@gmail.com>
2022-07-09 01:52:26 +0300

    nav changes made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import react, {Fragment} from 'react';
 import './App.css';
-import Header from './components/Header';
+import {Home} from './views/index';
+import { Header } from './components/index';
+import { BrowserRouter  as Router,Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-   <Fragment>
-     <Header />
-   </Fragment>
+    <Fragment>
+      <Header/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+          
+      </Router>
+    </Fragment>
+     
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,27 @@
 import react, {Fragment} from "react";
+import { Link } from "react-router";
 import logo from "../assets/images/Shipam-logo.png"
 import style from "../components/css/Header.module.css"
 import {Button} from "../components/index.js"
 
 const Header = () => {
+    const home = "/";
     return ( 
         <Fragment>
             <nav>
                 <div className={style.logo}>
-                    <img src={logo} alt="shipam-logo"/>
+                    <a href='/'>
+                      <img src={logo} alt="shipam-logo"/>
+                    </a>
                 </div>
                 <div>
                     <ul>
-
+                       <li></li>
                     </ul>
                 </div>
                 <div>
-                  <Button text="S" />
+                  <Button text="Sign In" />
+                  <Button text="Sign Up" />
                 </div>
             </nav>
         </Fragment>
