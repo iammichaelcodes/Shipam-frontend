5f13e96c52f2640f7aff62c0e6635968257cfc15
iammichaelcodes <mikeevboumwan52@gmail.com>
2022-07-08 23:50:51 +0300

    shipam commits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import react, {Fragment} from "react";
-
-
+import logo from "../assets\images/Shipam-logo.png"
+import style from "../components/css/Header"
 const Header = () => {
     return ( 
         <Fragment>
             <nav>
-                
+                <div className={style.logo}>
+                    <img src={logo} alt="shipam-logo"/>
+                </div>
             </nav>
         </Fragment>
      );
