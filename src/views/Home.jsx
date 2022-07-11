d9ee8667f66353cb1aad13b1e2bf9f940ee87100
iammichaelcodes <mikeevboumwan52@gmail.com>
2022-07-12 02:27:40 +0300

    banner Section design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,12 +1,19 @@
 import { Fragment } from "react";
-import style from "../views/css/Home.module.css";
+import style from "../views/css/Home.module.css"; 
+import bannerImg from "../assets/images/BannerDiv.png";
 
 const Home = () =>{
     return(
         <Fragment>
             <div className={style.home}>
-                <section className={style.bannerDiv}>
-                    <p>Home</p>
+                <section className={style.bannerDiv} style={{background:"url('../assets/images/BannerDiv.png')"}}>
+                    <div className={style.bannerCon}>
+                        <div>
+                           <h1>Find and Compare Discounted shipping rates</h1> 
+                           <p> Use Shipam to compare prices from multiple delivery providers without sign up. 
+                             Save up to 40% on your shipping cost with Shipbubble</p>
+                        </div>
+                    </div>
                 </section>
             </div>
             
