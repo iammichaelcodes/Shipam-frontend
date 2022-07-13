f2f49f64ff9422215b2f59c78f7b3a8ccafada95
iammichaelcodes <mikeevboumwan52@gmail.com>
2022-07-13 18:48:14 +0300

    sessions added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import style from "../views/css/Home.module.css"; 
 import location from "../assets/images/locationTrack.png";
 import courier from "../assets/images/CourierNetwork.png";
+import session3Image from "../assets/images/ShippingPartner.png";
 import Ease from "../assets/images/EaseOfAcess.png";
 import Flexibility from "../assets/images/flexibility.png";
 import Efficiency from "../assets/images/Efficiency.png";
@@ -63,29 +64,29 @@ const Home = () =>{
                           </div>
                            <div>
                                <div>
-                                    <img src={courier} alt="shipping Options" />
+                                    <img src={courier} alt="shipping Options" className={style.sessionImages} />
                                </div>
                                <div>
                                    <div>
                                       <h2>Multi-Courier Network</h2>
-                                      <p>Multi-Courier Network Get access to discounted rates from 
+                                      <p>Get access to discounted rates from 
                                         reliable shipping providers through our multi-courier network.</p>
                                    </div>
                                    <div className={style.courierDivList}>
-                                        <h3>Here's what you get </h3>
+                                        <h3>Here's what you get: </h3>
                                         <div>
-                                           <div>
-                                               <img src={Ease} alt="ease of access icon" />
+                                           <div className={style.icon}>
+                                               <img src={Ease} alt="ease of access icon" className={style.courierDivListIcon} />
                                                 <h3>Ease of Access</h3>
                                                  <p>Multiple delivery options all in one place</p>   
                                            </div>
-                                           <div>
-                                               <img src={Flexibility} alt="flexibility icon" />
+                                           <div className={style.icon}>
+                                               <img src={Flexibility} alt="flexibility icon" className={style.courierDivListIcon} />
                                                 <h3>Flexibility</h3>
                                                  <p>Tailor your delivery experience to what you prioritize, whether cost or delivery time.</p>   
                                            </div>
-                                           <div>
-                                               <img src={Efficiency} alt="efficiency icon" />
+                                           <div className={style.icon}>
+                                               <img src={Efficiency} alt="efficiency icon" className={style.courierDivListIcon} />
                                                 <h3>Efficiency</h3>
                                                 <p>Manage how your products are delivered in little to no time.</p>   
                                            </div>
@@ -97,8 +98,84 @@ const Home = () =>{
                 </section>
                 <section className={style.partnerDiv}>
                    <div>
-                    
+                        <div>
+                            <div>
+                                <h2>Dedicated Shipping Partner</h2>
+                                <p>
+                                  At little to no cost, have full control of your orders with us. Schedule orders, 
+                                  plan ahead with deliveries, and offer options based on your customers' needs.
+                                </p>
+                            </div>
+                            <div className={style.courierDivList}>
+                                <h3>Here's what you get: </h3>
+                                <div>
+                                    <div className={style.icon}>
+                                        <img src={Ease} alt="ease of access icon" className={style.courierDivListIcon} />
+                                        <h3>Ease of Access</h3>
+                                            <p>Multiple delivery options all in one place</p>   
+                                    </div>
+                                    <div className={style.icon}>
+                                        <img src={Flexibility} alt="flexibility icon" className={style.courierDivListIcon} />
+                                        <h3>Flexibility</h3>
+                                            <p>Tailor your delivery experience to what you prioritize, whether cost or delivery time.</p>   
+                                    </div>
+                                    <div className={style.icon}>
+                                        <img src={Efficiency} alt="efficiency icon" className={style.courierDivListIcon} />
+                                        <h3>Efficiency</h3>
+                                        <p>Manage how your products are delivered in little to no time.</p>   
+                                    </div>
+                                </div>
+                            </div>
+                        </div> 
+                        <div>
+                            <img src={session3Image} alt="shipping Options" className={style.sessionImages} />
+                        </div>    
                    </div>
+                </section>
+                <section className={style.AnalyticsDiv}>
+                      <div>
+                            <div>
+                                <img src={courier} alt="shipping Options" className={style.sessionImages} />
+                            </div>
+                            <div>
+                                <div>
+                                    <h2>Multi-Courier Network</h2>
+                                    <p>Get access to discounted rates from 
+                                    reliable shipping providers through our multi-courier network.</p>
+                                </div>
+                                <div className={style.courierDivList}>
+                                    <h3>Here's what you get: </h3>
+                                    <div>
+                                        <div className={style.icon}>
+                                            <img src={Ease} alt="ease of access icon" className={style.courierDivListIcon} />
+                                            <h3>Ease of Access</h3>
+                                                <p>Multiple delivery options all in one place</p>   
+                                        </div>
+                                        <div className={style.icon}>
+                                            <img src={Flexibility} alt="flexibility icon" className={style.courierDivListIcon} />
+                                            <h3>Flexibility</h3>
+                                                <p>Tailor your delivery experience to what you prioritize, whether cost or delivery time.</p>   
+                                        </div>
+                                        <div className={style.icon}>
+                                            <img src={Efficiency} alt="efficiency icon" className={style.courierDivListIcon} />
+                                            <h3>Efficiency</h3>
+                                            <p>Manage how your products are delivered in little to no time.</p>   
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                      </div>
+                </section>
+                <section >
+                    <div>
+                         <div>
+                            <h1>Why Shipam?</h1>
+                             <p></p>
+                         </div>
+                         <div>
+                             
+                         </div>
+                    </div>
                 </section>
             </div>
             
